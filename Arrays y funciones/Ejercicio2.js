9e2243c92f99056e1c2e85842879a072840cfd59
Sofía Cobo <sofiamariacobo@gmail.com>
2022-03-22 20:13:44 -0300

    Nuevos ejercicios

--- Arrays y funciones/Ejercicio2.js
+++ Arrays y funciones/Ejercicio2.js
@@ -8,5 +8,20 @@
 // ● Escribe por pantalla el elemento que ocupa la segunda posición.
 // ● Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 
-let ciudad = prompt("Ingrese una ciudad:");
-let ciudades = [];
+let getCities = () => {
+    let cities = [];
+    let input = "";
+    do {
+        input = prompt("Ingresa una ciudad:");
+        input == null ? console.log(cities) : cities.push(input);
+    } while (input !== null);
+    document.write(`La longitud del arreglo es ${cities.length}. <br>`);
+    document.write(`La primera posición es ${cities[0]}, la tercera es ${cities[2]} y la última es ${cities[cities.length-1]}. <br>`);
+    cities.push("Paris");
+    console.log(cities);
+    document.write(`La segunda posición es ${cities[1]}. <br>`);
+    cities.splice(1,1,'Barcelona');
+    console.log(cities);
+}
+
+getCities();
